53d6f7d72599bf47123490a1986d564e3b3d5c52
aylz10 <aylz10@126.com>
2026-06-07 23:00:16 +0800

    fix late load mode

--- module/metamount.sh
+++ module/metamount.sh
@@ -2,6 +2,11 @@
 # Copyright (C) 2026 meta-magic_mount-rs developers
 # SPDX-License-Identifier: Apache-2.0
 
+# meta-overlayfs Module Mount Handler
+# This script is the entry point for dual-directory module mounting
+[ ! -f "/dev/.esred" ] && exit 0
+[ -f "/dev/.mounted" ] && exit 0
+touch /dev/.mounted
 MODDIR="${0%/*}"
 
 ABI=$(getprop ro.product.cpu.abi)
@@ -17,12 +22,16 @@ if [ ! -f "$BINARY" ]; then
   exit 1
 fi
 
-nohup $BINARY 2>&1
+#nohup $BINARY 2>&1
+nohup $BINARY >"/data/adb/magic_mount/mm.log" 2>&1
 
 EXIT_CODE=$?
-
+echo "Mount start" >"/data/adb/magic_mount/mm2.log"
 if [ "$EXIT_CODE" = 0 ]; then
   /data/adb/ksud kernel notify-module-mounted
+  echo "Mount completed successfully" >>"/data/adb/magic_mount/mm2.log"
+else
+  echo "Mount failed with exit code $EXIT_CODE" >>"/data/adb/magic_mount/mm2.log"
 fi
 
 exit $EXIT_CODE
